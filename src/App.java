6e8e3fa364e6b1187a417b2501844f48eaf4708b
albe0x <buglialberto@gmail.com>
2025-05-06 11:32:23 +0200

    Aggiunto fine gara

--- src/App.java
+++ src/App.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) {
-        new Corridore("Luca").start();
-        new Corridore("Mario").start();
-        new Corridore("Alessio").start();
-        new Corridore("Federico").start();
+    public static void main(String[] args) throws Exception{
+       ArrayList<Corridore> corridori = new ArrayList<>();
+        String nomi[] = {"Luca", "Mario" ,"Alessio", "Federico"};
+        for (String n : nomi){
+            corridori.add(new Corridore(n));
+        }
+    
+    for (Corridore c : corridori){
+        c.start();
     }
+
+    for (Corridore c : corridori){
+        c.join();
+    }
+
+    System.out.println("Gara finita");
+
+
+    }    
 }
